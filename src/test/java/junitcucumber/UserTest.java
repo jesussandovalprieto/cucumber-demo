b425c5f758ea9671fb3d0ecf4e163c39394a8a43
Jesus Sandoval <jesus-sp@outlook.com>
2019-05-16 16:07:47 -0500

    Update output filename
    Update output filename

--- src/test/java/junitcucumber/UserTest.java
+++ src/test/java/junitcucumber/UserTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".", glue = "junitcucumber", plugin = { "json:target/test-cucumber.json" })
+@CucumberOptions(features = ".", glue = "junitcucumber", plugin = { "json:target/cucumber.json" })
 public class UserTest {
 
 }
